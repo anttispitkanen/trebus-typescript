1b49024a0c31aa926b13d556cb010660a07340d9
anttispitkanen <antti.pitkanen@vincit.fi>
2017-07-28 19:00:11 +0300

    Reorder action creators to make more sense.

--- client/src/actions/index.tsx
+++ client/src/actions/index.tsx
@@ -2,54 +2,65 @@ import * as constants from '../constants';
 import { Hotspot, MyLocation } from '../types';
 
 /**
- * Adding and deleting hotspots
+ * Adding hotspots
  */
 export interface AddHotspot {
     type: constants.ADD_HOTSPOT;
     newHotspot: Hotspot;
 }
 
+export const addHotspot = (newHotspot: Hotspot): AddHotspot => ({
+    type: constants.ADD_HOTSPOT,
+    newHotspot
+});
+
+/**
+ * Deleting hotspots
+ */
 export interface DeleteHotspot {
     type: constants.DELETE_HOTSPOT;
     indexToDelete: number;
 }
 
+// DELETE HOTSPOT FROM HOTSPOT ARRAY BASED ON THE INDEX
+export const deleteHotspot = (indexToDelete: number): DeleteHotspot => ({
+    type: constants.DELETE_HOTSPOT,
+    indexToDelete: indexToDelete
+});
+
+/**
+ * Moving hotspots up
+ */
 export interface MoveHotspotUp {
     type: constants.MOVE_HOTSPOT_UP;
     indexToMove: number;
 }
 
+export const moveHotspotUp = (indexToMove: number): MoveHotspotUp => ({
+    type: constants.MOVE_HOTSPOT_UP,
+    indexToMove: indexToMove
+});
+
+/**
+ * Moving hotspots down
+ */
 export interface MoveHotspotDown {
     type: constants.MOVE_HOTSPOT_DOWN;
     indexToMove: number;
 }
 
-export type HotspotAction = AddHotspot 
-                            | DeleteHotspot 
-                            | MoveHotspotUp
-                            | MoveHotspotDown;
-
-export const addHotspot = (newHotspot: Hotspot): AddHotspot => ({
-    type: constants.ADD_HOTSPOT,
-    newHotspot
-})
-
-// DELETE HOTSPOT FROM HOTSPOT ARRAY BASED ON THE INDEX
-export const deleteHotspot = (indexToDelete: number): DeleteHotspot => ({
-    type: constants.DELETE_HOTSPOT,
-    indexToDelete: indexToDelete
-})
-
-// MOVE HOTSPOT UP/DOWN
-export const moveHotspotUp = (indexToMove: number): MoveHotspotUp => ({
-    type: constants.MOVE_HOTSPOT_UP,
-    indexToMove: indexToMove
-})
-
 export const moveHotspotDown = (indexToMove: number): MoveHotspotDown => ({
     type: constants.MOVE_HOTSPOT_DOWN,
     indexToMove: indexToMove
-})
+});
+
+/**
+ * Export HotspotAction types
+ */
+export type HotspotAction = AddHotspot
+                            | DeleteHotspot
+                            | MoveHotspotUp
+                            | MoveHotspotDown;
 
 /**
  * Updating my location
@@ -64,4 +75,4 @@ export type MyLocationAction = UpdateMyLocation;
 export const updateMyLocation = (newLocation: MyLocation): UpdateMyLocation => ({
     type: constants.UPDATE_MY_LOCATION,
     newLocation
-})
+});
